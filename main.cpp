f8c2198afb715bee52e89f45269f156decde1b76
GFD11 <Gerard.Francis.Donohue@students.ittralee.ie>
2023-04-11 20:31:04 +0000

    11-04-23

--- main.cpp
+++ main.cpp
@@ -2,12 +2,10 @@
 #include "C12832.h"
 #include "LM75B.h"
 
-
 C12832 lcd(p5, p7, p6, p8, p11);
-LM75B temp_sensor(p28,p27);
-Thread thread1, thread2, thread3;
+LM75B temp_sensor(p28, p27);
 
-// Create the local filesystem 
+// Create the local filesystem
 LocalFileSystem local("local");
 
 const int DATA_SIZE = 100;
@@ -18,61 +16,58 @@ float data[DATA_SIZE];
 int data_count = 0;
 float min_temp = 0.0f, max_temp = 0.0f, avg_temp = 0.0f;
 
-Thread min_thread, max_thread, avg_thread;
+Thread min_thread;
+Thread max_thread;
+Thread avg_thread;
 
-
-void calculate_min(){
-    while(1){
-        if(data_count>0){
-            min_temp=data[0];
-               for (int i = 1; i < data_count; i++) {
-                    if (data[i] < min_temp) {
-                        min_temp = data[i];
-                    }
-               }    
+void calculate_min() {
+    while (1) {
+        if (data_count > 0) {
+            min_temp = data[0];
+            for (int i = 1; i < data_count; i++) {
+                if (data[i] < min_temp) {
+                    min_temp = data[i];
+                }
+            }
         }
-    }
-  }
-   
-
-void calculate_max(){
-    while(1){
-        if(data_count>0){
-            max_temp=data[0];
-               for (int i = 1; i < data_count; i++) {
-                    if (data[i] < max_temp) {
-                        max_temp = data[i];
-                    }
-               }
-        }
+        Thread::wait(500);
     }
 }
-    
 
-
-void calculate_avg(){
-     while(1){
-        if(data_count>0){
-            avg_temp=data[0];
-               for (int i = 0; i < data_count; i++) {
-                    if (data[i] < max_temp) {
-                        avg_temp = data[i];
-                    }
-               }
+void calculate_max() {
+    while (1) {
+        if (data_count > 0) {
+            max_temp = data[0];
+            for (int i = 1; i < data_count; i++) {
+                if (data[i] > max_temp) {
+                    max_temp = data[i];
+                }
+            }
         }
+        Thread::wait(500);
     }
 }
-    
 
-int main(){
+void calculate_avg() {
+    while (1) {
+        if (data_count > 0) {
+            float sum = 0.0f;
+            for (int i = 0; i < data_count; i++) {
+                sum += data[i];
+            }
+            avg_temp = sum / data_count;
+        }
+        Thread::wait(500);
+    }
+}
 
-    min_thread.start(calculate_min);
-    max_thread.start(calculate_max);
-    avg_thread.start(calculate_avg);
-   
+int main() {
+    min_thread.start((calculate_min));
+    max_thread.start((calculate_max));
+    avg_thread.start((calculate_avg));
 
     Timer log_timer;
-    log_timer.start(); 
+    log_timer.start();
 
     while (1) {
         // check if it's time to log data
@@ -84,8 +79,8 @@ int main(){
             data[data_count] = temp;
             data_count++;
 
-           //Storage of data
-           FILE* fp = fopen(FILENAME, "a");
+            //Storage of data
+            FILE* fp = fopen(FILENAME, "a");
             if (fp != NULL) {
                 fprintf(fp, "%.2f\n", temp);
                 fclose(fp);
@@ -101,8 +96,8 @@ int main(){
                 i++;
             }
             fclose(fp);
-            data_count=i;
-               
+            data_count = i;
+
             // show temp data on LCD
             lcd.cls();
             lcd.locate(0,3);
@@ -110,9 +105,8 @@ int main(){
             lcd.printf("Max temp: %.2fC\n", max_temp);
             lcd.printf("Avg temp: %.2fC\n", avg_temp);
         }
-    
-     //wait for 1 second
-    wait(1);
-    }
 
+        // wait for 1 second
+        wait(1);
+    }
 }
